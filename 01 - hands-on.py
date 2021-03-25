ea99dc0984834fb385ddef741b0c0b287ccd7041
castorfou <guillaume.ramelet@gmail.com>
2021-03-25 18:05:20 +0100

    atari

--- 01 - hands-on.py
+++ 01 - hands-on.py
@@ -108,7 +108,7 @@ for i in range(1000):
 
 # # DQN lunarlander
 
-# In[ ]:
+# In[8]:
 
 
 import gym
@@ -144,6 +144,225 @@ for i in range(1000):
     obs, rewards, dones, info = env.step(action)
     env.render()
 
+
+# In[10]:
+
+
+# Create environment
+env = gym.make('LunarLander-v2')
+# Load the trained agent
+model = DQN.load("dqn_lunar", env=env)
+
+# Evaluate the agent
+# NOTE: If you use wrappers with your environment that modify rewards,
+#       this will be reflected here. To evaluate with original rewards,
+#       wrap environment in a "Monitor" wrapper before other wrappers.
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+
+# Enjoy trained agent
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+
+# ![lunar_module.gif](attachment:lunar_module.gif)
+
+# # PPO multiprocessing cartpole
+
+# For this one I have created the code in  `cartpole_ppo.py` and run it direcly in python.
+
+# In[ ]:
+
+
+import gym
+import numpy as np
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.utils import set_random_seed
+
+def make_env(env_id, rank, seed=0):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environments you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    def _init():
+        env = gym.make(env_id)
+        env.seed(seed + rank)
+        return env
+    set_random_seed(seed)
+    return _init
+
+if __name__ == '__main__':
+    env_id = "CartPole-v1"
+    num_cpu = 4  # Number of processes to use
+    # Create the vectorized environment
+    env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+
+    # Stable Baselines provides you with make_vec_env() helper
+    # which does exactly the previous steps for you:
+    # env = make_vec_env(env_id, n_envs=num_cpu, seed=0)
+
+    model = PPO('MlpPolicy', env, verbose=1)
+    model.learn(total_timesteps=25000)
+
+    obs = env.reset()
+    for _ in range(1000):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
+
+
+# ![cartpole_ppo.gif](attachment:cartpole_ppo.gif)
+
+# # Monitoring training using callback
+
+# In[1]:
+
+
+import os
+
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+from stable_baselines3 import TD3
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import BaseCallback
+
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print("Num timesteps: {}".format(self.num_timesteps))
+                print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print("Saving new best model to {}".format(self.save_path))
+                  self.model.save(self.save_path)
+
+        return True
+
+# Create log dir
+log_dir = "tmp/"
+os.makedirs(log_dir, exist_ok=True)
+
+# Create and wrap the environment
+env = gym.make('LunarLanderContinuous-v2')
+env = Monitor(env, log_dir)
+
+# Add some action noise for exploration
+n_actions = env.action_space.shape[-1]
+action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+# Because we use parameter noise, we should use a MlpPolicy with layer normalization
+model = TD3('MlpPolicy', env, action_noise=action_noise, verbose=0)
+# Create the callback: check every 1000 steps
+callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
+# Train the agent
+timesteps = 1e5
+model.learn(total_timesteps=int(timesteps), callback=callback)
+
+plot_results([log_dir], timesteps, results_plotter.X_TIMESTEPS, "TD3 LunarLander")
+plt.show()
+
+
+# # Atari game pong
+
+# In[1]:
+
+
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3 import A2C
+
+# There already exists an environment generator
+# that will make and wrap atari environments correctly.
+# Here we are also multi-worker training (n_envs=4 => 4 environments)
+env = make_atari_env('PongNoFrameskip-v4', n_envs=6, seed=0)
+# Frame-stacking with 4 frames
+env = VecFrameStack(env, n_stack=4)
+
+model = A2C('CnnPolicy', env, verbose=1)
+model.learn(total_timesteps=25000)
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+
+# ![sb3_pong.gif](attachment:sb3_pong.gif)
+
+# # Atari game breakout
+
+# In[1]:
+
+
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3 import A2C
+
+# There already exists an environment generator
+# that will make and wrap atari environments correctly.
+# Here we are also multi-worker training (n_envs=4 => 4 environments)
+env = make_atari_env('Breakout-v0', n_envs=6, seed=0)
+# Frame-stacking with 4 frames
+env = VecFrameStack(env, n_stack=4)
+
+model = A2C('CnnPolicy', env, verbose=1)
+model.learn(total_timesteps=25000)
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+
+# ![sb3_breakout.gif](attachment:sb3_breakout.gif)
 
 # In[ ]:
 
