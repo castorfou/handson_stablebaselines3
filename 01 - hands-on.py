691b9d4f1a4f3721c0cb685010c76b4ef27bd760
castorfou <guillaume.ramelet@gmail.com>
2021-03-26 16:24:56 +0100

    run most of doc exampels

--- 01 - hands-on.py
+++ 01 - hands-on.py
@@ -364,6 +364,168 @@ while True:
 
 # ![sb3_breakout.gif](attachment:sb3_breakout.gif)
 
+# # pybullet
+
+# In[5]:
+
+
+import os
+import gym
+import pybullet_envs
+
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3 import PPO
+
+env = DummyVecEnv([lambda: gym.make("HalfCheetahBulletEnv-v0")])
+# Automatically normalize the input features and reward
+env = VecNormalize(env, norm_obs=True, norm_reward=True,
+                   clip_obs=10.)
+
+model = PPO('MlpPolicy', env)
+model.learn(total_timesteps=2000)
+
+# Don't forget to save the VecNormalize statistics when saving the agent
+log_dir = "./tmp/"
+model.save(log_dir + "ppo_halfcheetah")
+stats_path = os.path.join(log_dir, "vec_normalize.pkl")
+env.save(stats_path)
+
+# To demonstrate loading
+del model, env
+
+# Load the saved statistics
+env = DummyVecEnv([lambda: gym.make("HalfCheetahBulletEnv-v0")])
+env = VecNormalize.load(stats_path, env)
+#  do not update them at test time
+env.training = False
+# reward normalization is not needed at test time
+env.norm_reward = False
+
+# Load the agent
+model = PPO.load(log_dir + "ppo_halfcheetah", env=env)
+
+# obs = env.reset()
+# while True:
+#     action, _states = model.predict(obs)
+#     obs, rewards, dones, info = env.step(action)
+#     env.render()
+
+
+# # Hindsight Experience Replay (HER)
+
+# In[2]:
+
+
+import gym
+import highway_env
+import numpy as np
+
+from stable_baselines3 import HER, SAC, DDPG, TD3
+from stable_baselines3.common.noise import NormalActionNoise
+
+env = gym.make("parking-v0")
+
+# Create 4 artificial transitions per real transition
+n_sampled_goal = 4
+
+# SAC hyperparams:
+model = HER(
+    "MlpPolicy",
+    env,
+    SAC,
+    n_sampled_goal=n_sampled_goal,
+    goal_selection_strategy="future",
+    # IMPORTANT: because the env is not wrapped with a TimeLimit wrapper
+    # we have to manually specify the max number of steps per episode
+    max_episode_length=100,
+    verbose=1,
+    buffer_size=int(1e6),
+    learning_rate=1e-3,
+    gamma=0.95,
+    batch_size=256,
+    online_sampling=True,
+    policy_kwargs=dict(net_arch=[256, 256, 256]),
+)
+
+model.learn(int(2e5))
+model.save("her_sac_highway")
+
+# Load saved model
+# Because it needs access to `env.compute_reward()`
+# HER must be loaded with the env
+model = HER.load("her_sac_highway", env=env)
+
+obs = env.reset()
+
+# Evaluate the agent
+episode_reward = 0
+for _ in range(100):
+    action, _ = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    episode_reward += reward
+    if done or info.get("is_success", False):
+        print("Reward:", episode_reward, "Success?", info.get("is_success", False))
+        episode_reward = 0.0
+        obs = env.reset()
+
+
+# In[1]:
+
+
+import gym
+import highway_env
+import numpy as np
+
+from stable_baselines3 import HER, SAC, DDPG, TD3
+from stable_baselines3.common.noise import NormalActionNoise
+# Load saved model
+# Because it needs access to `env.compute_reward()`
+# HER must be loaded with the env
+
+env = gym.make("parking-v0")
+
+model = HER.load("her_sac_highway", env=env)
+
+obs = env.reset()
+
+# Evaluate the agent
+episode_reward = 0
+for _ in range(100):
+    action, _ = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    episode_reward += reward
+    if done or info.get("is_success", False):
+        print("Reward:", episode_reward, "Success?", info.get("is_success", False))
+        episode_reward = 0.0
+        obs = env.reset()
+
+
+# # make a gif
+
+# In[2]:
+
+
+import imageio
+import numpy as np
+
+from stable_baselines3 import A2C
+
+model = A2C("MlpPolicy", "LunarLander-v2").learn(1000000)
+
+images = []
+obs = model.env.reset()
+img = model.env.render(mode='rgb_array')
+for i in range(350):
+    images.append(img)
+    action, _ = model.predict(obs)
+    obs, _, _ ,_ = model.env.step(action)
+    img = model.env.render(mode='rgb_array')
+
+imageio.mimsave('lander_a2c.gif', [np.array(img) for i, img in enumerate(images) if i%2 == 0], fps=29)
+
+
 # In[ ]:
 
 
