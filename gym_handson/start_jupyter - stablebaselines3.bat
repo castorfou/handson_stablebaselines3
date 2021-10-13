5c96d5307057aede777455d3b1a929467fc4d157
castorfou <guillaume.ramelet@gmail.com>
2021-10-13 14:13:00 +0200

    refactoring

--- gym_handson/start_jupyter - stablebaselines3.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-set Path=C:\Users\F279814\AppData\Local\Continuum\anaconda3;C:\Users\F279814\AppData\Local\Continuum\anaconda3\Library\mingw-w64\bin;C:\Users\F279814\AppData\Local\Continuum\anaconda3\Library\usr\bin;C:\Users\F279814\AppData\Local\Continuum\anaconda3\Library\bin;C:\Users\F279814\AppData\Local\Continuum\anaconda3\Scripts;C:\Users\F279814\AppData\Local\Continuum\anaconda3\bin;C:\Users\F279814\AppData\Local\Continuum\anaconda3\condabin;%PATH%
-REM open conda command prompt
-d:
-cd D:\git\handson_stablebaselines3\gym_handson
-call conda activate stablebaselines3 
-jupyter notebook
